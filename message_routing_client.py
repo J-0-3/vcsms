98d9d43e6329c2a30c5767f4ee938ca743d901a7
joedthomas2005 <joedt100@protonmail.com>
2022-10-10 23:55:04 +0100

    began working on sha256 implementation following the NIST FIPS 180-4 specification. got as far as creating the message schedule.

--- message_routing_client.py
+++ message_routing_client.py
@@ -8,69 +8,72 @@ def recv_thread(sock, message_queue, request_queue, stopped):
     while not stopped():
         data = sock.recv(2048)
         if data[:3] == b'MSG':
-            message = data[3:].split(b'DAT',1) 
-            message_queue.put((str(message[0], 'utf-8'), str(message[1], 'utf-8')))
+            recipient, msg_data = data[3:].split(b'DAT', 1)
+            message_queue.put((str(recipient[0], 'utf-8'), str(msg_data[1], 'utf-8')))
         else:
             request_queue.put(data)
 
+
 def message_process_thread(message_queue, stopped):
     while not stopped():
-            if not message_queue.empty():
-                message = message_queue.get()
-                print(f"\rNEW MESSAGE: \n{message[1]}\n\nFROM: {message[0]}")
+        if not message_queue.empty():
+            recipient, data = message_queue.get()
+            print(f"\rNEW MESSAGE: \n{str(data, 'utf-8')}\n\nFROM: {str(recipient, 'utf-8')}")
 
 
 def send_thread(sock, request_queue, send_queue):
     while True:
-        message = send_queue.get()
-        receiver = message[0]
-        data = message[1]
+        receiver, data = send_queue.get()
         if receiver == "QUIT":
             sock.send(b'QUIT')
             break
-        sock.send(b'TO'+receiver.encode())
-        if request_queue.get() == b'data': # queue.get() blocks until a piece of data is available
+        sock.send(b'TO' + receiver.encode())
+        if request_queue.get() == b'data':  # queue.get() blocks until a piece of data is available
             sock.send(data.encode())
             if request_queue.get() == b'OK':
                 print("Message Sent Successfully")
             else:
                 print("server indicated error in sending message")
 
-        else:    
+        else:
             print(f"server rejected message request to {receiver}")
+
 
 class Messenger:
     def __init__(self):
+        self.t_recv = None
+        self.t_msg = None
+        self.t_send = None
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.msg_queue = Queue()
         self.send_queue = Queue()
         self.req_queue = Queue()
         self.stopped = False
 
-    def connect(self, ip:str, port:int, username:str):
+    def connect(self, ip: str, port: int, username: str):
         self.s.connect((ip, port))
         self.s.send(name.encode())
         status = self.s.recv(1024)
         if status != b'OK':
             self.s.close()
             return status
-        
+
         self.t_send = Thread(
-                target=send_thread,
-                args=(self.s, self.req_queue, self.send_queue)
+            target=send_thread,
+            args=(self.s, self.req_queue, self.send_queue)
         )
         self.t_msg = Thread(
-                target=message_process_thread,
-                args=(self.msg_queue, lambda: self.stopped)
+            target=message_process_thread,
+            args=(self.msg_queue, lambda: self.stopped)
         )
         self.t_recv = Thread(
-                target=recv_thread,
-                args=(self.s, self.msg_queue, self.req_queue, lambda:self.stopped)
+            target=recv_thread,
+            args=(self.s, self.msg_queue, self.req_queue, lambda: self.stopped)
         )
         self.t_send.start()
         self.t_msg.start()
         self.t_recv.start()
-    
+
     def exit(self):
         self.send_queue.put(("QUIT", None))
         self.t_send.join()
@@ -79,15 +82,16 @@ class Messenger:
         self.t_recv.join()
         self.s.close()
 
-    def send(self, to: str, message: str):
-        self.send_queue.put((to, message))
+    def send(self, to: str, text: str):
+        self.send_queue.put((to, text))
 
     def read(self, block=True):
-    
+
         if not self.msg_queue.empty() or block:
             return self.msg_queue.get()
         else:
-            return (None, None)
+            return None, None
+
 
 if __name__ == "__main__":
     name = input("What is your name?")
--- rsa/primes.py
+++ rsa/primes.py
@@ -1,19 +1,22 @@
 import math
 import random
+
+
 def sieve_of_eratosthenes(maximum: int):
     primes = [0] * (maximum - 1)
-    #0th index = 2
+    # 0th index = 2
     i = 2
     while i < math.sqrt(maximum):
         if primes[i - 2] == 0:
-            for j in range(i**2, maximum+1, i):
+            for j in range(i ** 2, maximum + 1, i):
                 primes[j - 2] = 1
         i += 1
-    
-    return [x+2 for x in filter(lambda i: primes[i] == 0, range(len(primes)))]
+
+    return [x + 2 for x in filter(lambda i: primes[i] == 0, range(len(primes)))]
+
 
 # def miller_rabin_primality_test(x: int):
-    
+
 
 def is_prime(x: int):
     prime = True
@@ -23,12 +26,13 @@ def is_prime(x: int):
     if p > 10000:
         return False
     return prime
+
+
 if __name__ == "__main__":
     while True:
         count = 1
         while True:
             if is_prime(random.randrange(1, 10000)):
-                    print(f"prime found in {count} attempts")
-                    break
+                print(f"prime found in {count} attempts")
+                break
             count += 1
-
