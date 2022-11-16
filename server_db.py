4d404723af4e14f77776bbc9161a2e229c737fed
joseph <joedt100@gmail.com>
2022-11-16 10:07:45 +0000

    more db

--- server_db.py
+++ server_db.py
@@ -3,6 +3,10 @@ import sqlite3
 
 class Server_DB:
     def __init__(self, path=':memory:'):
-        try:
-            self.db = sqlite3.connect(path)
-        except FileNotFoundError
+        self.db = sqlite3.connect(path)
+        self.cursor = self.db.cursor()
+    def setup_db(self):
+        self.cursor.execute("create table if not exists public_keys (id text, exponent integer, modulus integer)")
+        self.cursor.execute("create table if not exists connection_log (id text, time text)")
+        self.cursor.execute("create table if not exists logged_in (id text, connected integer)")
+    def user_login(self, id: str, pubkey: tuple, ):
