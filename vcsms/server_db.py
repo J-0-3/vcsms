09b78077a1df7574db2bc7277350fdc1b7679d27
joseph thomas <joedt100@protonmail.com>
2022-12-27 01:44:23 +0000

    Update server_db.py

--- vcsms/server_db.py
+++ vcsms/server_db.py
@@ -2,10 +2,12 @@ import sqlite3
 import os
 from . import keys
 
+
 class Server_DB:
     def __init__(self, path='server.db', pubkey_directory='client_public_keys'):
         self.db = sqlite3.connect(path)
         self.pubkeys = pubkey_directory
+
 
     def setup_db(self):
         self.db.execute("create table if not exists connection_log (id text, time text)")
@@ -13,11 +15,12 @@ class Server_DB:
         self.db.execute("update logged_in set connected=0")
         self.db.commit()
 
+
     def user_known(self, id: str) -> bool:
         return os.path.exists(os.path.join(self.pubkeys, id))
 
-    def user_login(self, id: str, pubkey: tuple):
 
+    def user_login(self, id: str, pubkey: tuple):
         if not self.user_known(id):
             keys.write_key(pubkey, os.path.join(self.pubkeys, id))
 
@@ -25,9 +28,11 @@ class Server_DB:
         self.db.execute("replace into logged_in values(?, 1)", (id, ))
         self.db.commit()
 
+
     def user_logout(self, id: str):
         self.db.execute("replace into logged_in values(?, 0)", (id, ))
         self.db.commit()
+
 
     def is_logged_in(self, id: str) -> bool:
         cursor = self.db.execute("select connected from logged_in where id=?", (id, ))
@@ -36,13 +41,13 @@ class Server_DB:
             return False
         return True
 
-    def get_pubkey(self, id: str) -> tuple:
 
+    def get_pubkey(self, id: str) -> tuple:
         if os.path.exists(os.path.join(self.pubkeys, id)):
             key = keys.load_key(os.path.join(self.pubkeys, id))
             return key
-        else:
-            raise Exception("User not found")
+        raise Exception("User not found")
+
 
     def close(self):
         self.db.close()
