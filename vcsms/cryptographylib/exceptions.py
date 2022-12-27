bca581c8c287be818b1c36d8b9f7a11c57b13d65
joseph thomas <joedt100@protonmail.com>
2022-12-27 01:47:40 +0000

    Update exceptions.py

--- vcsms/cryptographylib/exceptions.py
+++ vcsms/cryptographylib/exceptions.py
@@ -2,6 +2,7 @@ class CryptographyException(Exception):
     def __init__(self, message):
         super().__init__(f"Cryptographic failure: {message}")
 
+
 class DecryptionFailureException(CryptographyException):
     def __init__(self, key: int):
         super().__init__(f"Failed to decrypt data using key {key}")
