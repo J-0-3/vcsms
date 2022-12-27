8ba1784b58b44ae072becef24367058bcab1524d
joseph thomas <joedt100@protonmail.com>
2022-12-27 02:01:50 +0000

    Update signing.py

--- vcsms/signing.py
+++ vcsms/signing.py
@@ -22,7 +22,7 @@ def verify(data: bytes, signature: bytes, pub_key: tuple) -> bool:
     return False
 
 
-def gen_signed_diffie_hellman(dh_private_key: int, rsa_private_key: tuple, dh_group: tuple, message_id: int = 0) -> tuple[int,bytes]:
+def gen_signed_diffie_hellman(dh_private_key: int, rsa_private_key: tuple, dh_group: tuple, message_id: int = 0) -> tuple[int, bytes]:
     dh_public_key = dhke.generate_public_key(dh_private_key, dh_group)
     dh_public_key_hex = hex(dh_public_key)[2:].encode()
     message_id_hex = hex(message_id)[2:].encode()
