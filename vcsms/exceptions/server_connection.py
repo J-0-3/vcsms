a1d20531cf46f11e3751705fd9716becde342001
joseph thomas <joedt100@protonmail.com>
2022-12-27 01:47:05 +0000

    Update server_connection.py

--- vcsms/exceptions/server_connection.py
+++ vcsms/exceptions/server_connection.py
@@ -2,13 +2,16 @@ class ConnectionException(Exception):
     def __init__(self, message: str):
         super().__init__(f"Failure connecting to server: {message}")
 
+
 class MalformedPacketException(ConnectionException):
     def __init__(self):
         super().__init__("Malformed packet")
 
+
 class PublicKeyIdMismatchException(ConnectionException):
     def __init__(self, public_key_fp, server_fp):
         super().__init__(f"Public key fingerprint {public_key_fp} did not match server fingerprint {server_fp}")
+
 
 class SignatureVerifyFailureException(ConnectionException):
     def __init__(self, signature: bytes):
