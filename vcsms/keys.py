56837ade2a15ede86afd47e9a7b9923f4c2114ff
joseph thomas <joedt100@protonmail.com>
2022-12-27 01:24:20 +0000

    Update keys.py

--- vcsms/keys.py
+++ vcsms/keys.py
@@ -1,10 +1,10 @@
 from .cryptographylib import rsa, sha256
-from . import signing
 
 
 def write_key(key: tuple, out: str):
     with open(out, 'w') as f:
         f.write(f"{hex(key[0])[2:]}:{hex(key[1])[2:]}")
+
 
 def generate_keys(pub_out: str, priv_out: str) -> tuple[tuple[int, int], tuple[int, int]]:
     pub, priv = rsa.gen_keypair(2048)
@@ -12,11 +12,13 @@ def generate_keys(pub_out: str, priv_out: str) -> tuple[tuple[int, int], tuple[i
     write_key(priv, priv_out)
     return pub, priv
 
+
 def load_key(path: str) -> tuple[int, int]:
     with open(path, 'r') as f:
         exp, mod = f.read().split(':')
         key = (int(exp, 16), int(mod, 16))
     return key
+
 
 def fingerprint(key: tuple) -> str:
     hash = sha256.hash(hex(key[0])[2:].encode() + hex(key[1])[2:].encode())
