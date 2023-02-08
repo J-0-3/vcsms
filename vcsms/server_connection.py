ad8b3c9f70d18990cc1951f5b60d335ae09997a9
joseph thomas <joedt100@protonmail.com>
2023-02-08 10:06:31 +0000

    Update server_connection.py

--- vcsms/server_connection.py
+++ vcsms/server_connection.py
@@ -7,8 +7,8 @@ from . import keys
 from . import signing
 from .non_stream_socket import NonStreamSocket
 from .logger import Logger
-from .cryptographylib import dhke, sha256, utils, aes256
-from .cryptographylib.exceptions import DecryptionFailureException
+from .cryptography import dhke, sha256, utils, aes256
+from .cryptography.exceptions import DecryptionFailureException
 from .exceptions.server_connection import *
 
 
